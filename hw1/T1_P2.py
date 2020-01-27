39f5effddfb88aa46a4c1b6992bb73cdbde9111c
JKinx <jambay606@yahoo.com>
2020-01-27 03:33:47 -0500

    hw added

---
+++ hw1/T1_P2.py
@@ -0,0 +1,75 @@
+import math
+import matplotlib.cm as cm
+
+from math import exp
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.colors as c
+
+# Read from file and extract X and y
+df = pd.read_csv('data/p2.csv')
+
+X_df = df[['x1', 'x2']]
+y_df = df['y']
+
+X = X_df.values
+y = y_df.values
+
+def predict_kernel(alpha=0.1):
+    """Returns predictions using kernel-based predictor with the specified alpha."""
+    # TODO: your code here
+    return y_df
+
+def predict_knn(k=1):
+    """Returns predictions using KNN predictor with the specified k."""
+    # TODO: your code here
+    return y_df
+
+def plot_kernel_preds(alpha):
+    title = 'Kernel Predictions with alpha = ' + str(alpha)
+    plt.figure()
+    plt.title(title)
+    plt.xlabel('x1')
+    plt.ylabel('x2')
+    plt.xlim((0, 2))
+    plt.ylim((0, 2))
+
+    plt.xticks(np.arange(0, 2, 0.1))
+    plt.yticks(np.arange(0, 2, 0.1))
+    y_pred = predict_kernel(alpha)
+    print(y_pred)
+    norm = c.Normalize(vmin=0.,vmax=1.)
+    plt.scatter(df['x1'], df['x2'], c=y_pred, cmap='gray', vmin=0, vmax = 1, edgecolors='b')
+
+    # Saving the image to a file, and showing it as well
+    plt.savefig(title + '.png')
+    plt.show()
+
+def plot_knn_preds(k):
+    title = 'KNN Predictions with k = ' + str(k)
+    plt.figure()
+    plt.title(title)
+    plt.xlabel('x1')
+    plt.ylabel('x2')
+    plt.xlim((0, 2))
+    plt.ylim((0, 2))
+
+    plt.xticks(np.arange(0, 2, 0.1))
+    plt.yticks(np.arange(0, 2, 0.1))
+    y_pred = predict_knn(k)
+    print(y_pred)
+    norm = c.Normalize(vmin=0.,vmax=1.)
+    plt.scatter(df['x1'], df['x2'], c=y_pred, cmap='gray', vmin=0, vmax = 1, edgecolors='b')
+
+    # Saving the image to a file, and showing it as well
+    plt.savefig(title + '.png')
+    plt.show()
+
+for alpha in (0.1, 10, 100):
+    # TODO: Print the loss for each chart.
+    plot_kernel_preds(alpha)
+
+for k in (1, 4, 15):
+    # TODO: Print the loss for each chart.
+    plot_knn_preds(k)
